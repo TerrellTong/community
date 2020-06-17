041d5da8bf1e5d5edee0ee397b3b080da080b509
TerrellTong <852507537@qq.com>
2020-06-17 11:59:00 +0800

    注册功能完成

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -28,7 +28,7 @@ public class HomeController {
     @RequestMapping(path="/index",method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page){
         //在方法调用前，SpringMVC会自动实例化Model和Page，并将Page注入Model.
-        //所以，在thymeleaf中可以直接访问Page对象中的数据
+        //所以，在thymeleaf中可以直接访问Page对象中的数据(相当于page已经存在了Model中了)
         page.setRows(discussPostService.findDiscussPostRows(0));
         //分页链接的请求/index?current=xxx ,current是存储在了page中
         page.setPath("/index");
